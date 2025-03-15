90454d97a79695fceaa924c4a0bdc0595d36d712
octaview <kprinzin@gmail.com>
2025-03-15 11:34:28 +0300

    added functionality for batch adding and removing tags from cards, as well as methods for getting the number of cards and tags by their IDs

--- internal/service/card-label_service.go
+++ internal/service/card-label_service.go
@@ -21,6 +21,7 @@ type CardLabelService struct {
 	cardRepo      repository.CardRepository
 	labelRepo     repository.LabelRepository
 	boardRepo     repository.BoardRepository
+	columnRepo    repository.ColumnRepository
 }
 
 func NewCardLabelService(
@@ -28,12 +29,14 @@ func NewCardLabelService(
 	cardRepo repository.CardRepository,
 	labelRepo repository.LabelRepository,
 	boardRepo repository.BoardRepository,
+	columnRepo repository.ColumnRepository,
 ) *CardLabelService {
 	return &CardLabelService{
 		cardLabelRepo: cardLabelRepo,
 		cardRepo:      cardRepo,
 		labelRepo:     labelRepo,
 		boardRepo:     boardRepo,
+		columnRepo:    columnRepo,
 	}
 }
 
@@ -125,8 +128,120 @@ func (s *CardLabelService) getColumnForCard(ctx context.Context, card *models.Ca
 }
 
 func (s *CardLabelService) getColumnByID(ctx context.Context, columnID uint) (*models.Column, error) {
-	column := &models.Column{}
-	column.ID = columnID
+	column, err := s.columnRepo.GetByID(ctx, columnID)
+	if err != nil {
+		if errors.Is(err, models.ErrColumnNotFound) {
+			return nil, models.ErrColumnNotFound
+		}
+		return nil, fmt.Errorf("failed to get column: %w", err)
+	}
 	
 	return column, nil
+}
+
+func (s *CardLabelService) BatchAddLabelsToCard(ctx context.Context, cardID uint, labelIDs []uint) error {
+	card, err := s.cardRepo.GetByID(ctx, cardID)
+	if err != nil {
+		if errors.Is(err, models.ErrCardNotFound) {
+			return models.ErrCardNotFound
+		}
+		return err
+	}
+	
+	column, err := s.getColumnForCard(ctx, card)
+	if err != nil {
+		return err
+	}
+	
+	for _, labelID := range labelIDs {
+		label, err := s.labelRepo.GetByID(ctx, labelID)
+		if err != nil {
+			if errors.Is(err, models.ErrLabelNotFound) {
+				return fmt.Errorf("label %d not found", labelID)
+			}
+			return err
+		}
+		
+		if label.BoardID != column.BoardID {
+			return fmt.Errorf("label %d belongs to board %d but card %d belongs to board %d", 
+				labelID, label.BoardID, cardID, column.BoardID)
+		}
+	}
+	
+	for _, labelID := range labelIDs {
+		if err := s.cardLabelRepo.AddLabelToCard(ctx, cardID, labelID); err != nil {
+			return err
+		}
+	}
+	
+	return nil
+}
+
+func (s *CardLabelService) BatchRemoveLabelsFromCard(ctx context.Context, cardID uint, labelIDs []uint) error {
+	_, err := s.cardRepo.GetByID(ctx, cardID)
+	if err != nil {
+		if errors.Is(err, models.ErrCardNotFound) {
+			return models.ErrCardNotFound
+		}
+		return err
+	}
+	
+	for _, labelID := range labelIDs {
+		_, err := s.labelRepo.GetByID(ctx, labelID)
+		if err != nil {
+			if errors.Is(err, models.ErrLabelNotFound) {
+				return fmt.Errorf("label %d not found", labelID)
+			}
+			return err
+		}
+	}
+	
+	for _, labelID := range labelIDs {
+		if err := s.cardLabelRepo.RemoveLabelFromCard(ctx, cardID, labelID); err != nil {
+			return err
+		}
+	}
+	
+	return nil
+}
+
+func (s *CardLabelService) GetCardCountByLabelID(ctx context.Context, labelID uint) (int, error) {
+	cards, err := s.GetCardsByLabelID(ctx, labelID)
+	if err != nil {
+		return 0, err
+	}
+	
+	return len(cards), nil
+}
+
+func (s *CardLabelService) GetLabelCountByCardID(ctx context.Context, cardID uint) (int, error) {
+	labels, err := s.GetLabelsByCardID(ctx, cardID)
+	if err != nil {
+		return 0, err
+	}
+	
+	return len(labels), nil
+}
+
+func (s *CardLabelService) RemoveAllLabelsFromCard(ctx context.Context, cardID uint) error {
+	_, err := s.cardRepo.GetByID(ctx, cardID)
+	if err != nil {
+		if errors.Is(err, models.ErrCardNotFound) {
+			return models.ErrCardNotFound
+		}
+		return err
+	}
+	
+	labels, err := s.GetLabelsByCardID(ctx, cardID)
+	if err != nil {
+		return err
+	}
+	
+	for _, label := range labels {
+		if err := s.cardLabelRepo.RemoveLabelFromCard(ctx, cardID, label.ID); err != nil {
+			return err
+		}
+	}
+	
+	return nil
 }
